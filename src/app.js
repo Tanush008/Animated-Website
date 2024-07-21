d4b0c76ea5745fc513549b842428cd520d1d516c
*Tanush008* <*agggarwal.tanush1208@gmail.com*>
2024-07-21 11:11:11 +0530

    Completed the Animation Part

--- src/app.js
+++ src/app.js
@@ -76,29 +76,67 @@ function teamAnimation() {
     });
   });
 }
-var clutter = "";
-document
-  .querySelector(".textPara")
-  .textContent.split("")
-  .forEach(function (e) {
-    if (e === " ") {
-      clutter += `<span>&nbsp;</span>`;
-    }
-    clutter += `<span>${e}</span>`;
-  });
-  document.querySelector(".textPara").innerHTML=clutter;
-  gsap.set(".textPara span",{opacity:.1})
-  gsap.to(".textPara span",{
-    scrollTrigger:{
-      trigger:".para",
-      start:"top 50%",
-      end:"bottom 80%",
-      scrub:1
+function textAnimation() {
+  var clutter = "";
+  document
+    .querySelector(".textPara")
+    .textContent.split("")
+    .forEach(function (e) {
+      if (e === " ") {
+        clutter += `<span>&nbsp;</span>`;
+      }
+      clutter += `<span>${e}</span>`;
+    });
+  document.querySelector(".textPara").innerHTML = clutter;
+  gsap.set(".textPara span", { opacity: 0.1 });
+  gsap.to(".textPara span", {
+    scrollTrigger: {
+      trigger: ".para",
+      start: "top 50%",
+      end: "bottom 80%",
+      scrub: 1,
     },
-    opacity:1,
-    ease:Power4,
-    stagger:.03
-  })
+    opacity: 1,
+    ease: Power4,
+    stagger: 0.03,
+  });
+}
+function loco() {
+  (function () {
+    const locomotiveScroll = new LocomotiveScroll();
+  })();
+}
+function capsules() {
+  gsap.to(".capsules :nth-child(2)", {
+    scrollTrigger: {
+      trigger: ".capsule",
+      start: "top 80%",
+      end: "bottom bottom",
+      scrub: 1,
+    },
+    ease: Power4,
+    y: 0,
+  });
+}
+function colorAnimation() {
+  document.querySelectorAll(".section").forEach(function (e) {
+    ScrollTrigger.create({
+      trigger: e,
+      start: "top 50%",
+      end: "bottom 50%",
+      onEnter: function () {
+        document.body.setAttribute("theme", e.dataset.color);
+      },
+      onEnterBack: function () {
+        document.body.setAttribute("theme", e.dataset.color);
+      },
+    });
+  });
+}
+loco();
+colorAnimation();
+textAnimation();
 teamAnimation();
 realAnimation();
 homeAnimation();
+capsules();
